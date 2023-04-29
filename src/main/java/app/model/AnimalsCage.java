29a82eafd0d1e02b40b583a94820dc903f156ac9
Maxibelo <maxibelo78@yandex.ru>
2023-04-29 14:53:30 +0300

    mess2

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -6,27 +6,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-    private Animal animal1;
-    private Animal animal2;
-    private Timer timer = new Timer();
-
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
 
     @Autowired
-    public AnimalsCage(@Qualifier("cat") Animal animal1,
-                       @Qualifier("dog") Animal animal2) {
-        this.animal1 = animal1;
-        this.animal2 = animal2;
-    }
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal1.toString() + ", " + animal2.toString());
+        System.out.println(animal.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
-
-
     public Timer getTimer() {
         return timer;
     }
